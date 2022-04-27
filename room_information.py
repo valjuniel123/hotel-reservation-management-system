a70a27555d56972964d39cda5e5a52e278019825
valjuniel123 <valjuniel123@gmail.com>
2022-04-27 22:21:06 +0800

    Working ComboBox

--- room_information.py
+++ room_information.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
 
+import humanize
+
 from subprocess import call
 
 import mysql.connector
@@ -12,37 +14,36 @@ def toLogin():
     print("Logged out!")
     main_screen.destroy()
     
-def toRoomInfo(roomNumber):
-    print("Room Number:", roomNumber, " Details")
-    # main_screen.withdraw()
-    room_selectInfo(roomNumber)
-    # main_screen.deiconify()
+def closeInfo():
+    roomSelected.destroy()
+
     
-def room_selectInfo(roomNumber):
-    
+def room_selectInfo(detailsArray):
     global roomSelected
     roomSelected = Toplevel(main_screen)
     roomSelected.title("Room Information")
     roomSelected.geometry("300x600")
     
-    roomNum = "Room " + str(roomNumber)
+    roomNum = "Room " + str(detailsArray[0][1])
     Label(roomSelected, text="Room Information", bg="blue", width="300", height="1", font=("Calibri", 20)).pack() 
     Label(roomSelected, text=roomNum, bg="blue", width="300", height="1", font=("Calibri", 20)).pack() 
- 
     
+    Button(roomSelected, text="Close", height="2", width="30", command=closeInfo).pack()
+    Label(text="").pack()    
+
     Label(roomSelected, text="").pack()
-    Label(roomSelected, text="Name\tMarco the Phoenix", anchor='w').pack(fill='both')
-    Label(roomSelected, text="").pack()
-    
-    Label(roomSelected, text="").pack()
-    Label(roomSelected, text="Name\tMarco the Phoenix", anchor='w').pack(fill='both')
-    Label(roomSelected, text="").pack()
-    
-    Label(roomSelected, text="").pack()
-    Label(roomSelected, text="Name\tMarco the Phoenix", anchor='w').pack(fill='both')
+    roomType = "Room Type:\t" + detailsArray[0][6]
+    Label(roomSelected, text=roomType, anchor='w').pack(fill='both')
     Label(roomSelected, text="").pack()
     
+    location = "Location:\t" + humanize.ordinal(detailsArray[0][4]) + " Floor"
+    Label(roomSelected, text=location, anchor='w').pack(fill='both')
     Label(roomSelected, text="").pack()
+    
+    roomStatus = "Status:\t\t" + "Not Occupied"
+    Label(roomSelected, text=roomStatus, anchor='w').pack(fill='both')
+    Label(roomSelected, text="").pack()
+    
     Label(roomSelected, text="Name\tMarco the Phoenix", anchor='w').pack(fill='both')
     Label(roomSelected, text="").pack()
     
@@ -57,11 +58,63 @@ def room_selectInfo(roomNumber):
     Label(roomSelected, text="").pack()
     Label(roomSelected, text="Name\tMarco the Phoenixs", anchor='w').pack(fill='both')
     Label(roomSelected, text="").pack()
+ 
+#SQL FOR ROOMS
+
+def getFloorsAvailable():
+    counter = 0
+    floorArray = []
+    converter = ""
     
+    query = "SELECT roomFloor FROM roominformation GROUP BY roomFloor ORDER BY roomFloor ASC"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    for row in result:
+        converter = humanize.ordinal(row[0]) + " Floor"
+        floorArray.insert(counter, converter)
+        counter+=1
+    return floorArray
+
+def floorChanged(index, value, op):
+    roomChoice = []
+    counter=0
+    print ("combobox changed to " + floorSelected.get())
+    
+    query = "SELECT * FROM roominformation WHERE roomFloor = '" + floorSelected.get() + "'"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    if(len(result) > 0):
+        for row in result:
+            print(row[0])
+            roomChoice.insert(counter, row[1])
+            counter+=1
+        selectRoom.config(value=roomChoice)
+            
+    else:
+        print("No rooms available")
+        messagebox.showwarning("Warning", "No rooms available")
+        login_screen.destroy()
+        
+def roomChanged(index, value, op):
+    detailsArray = []
+    counter=0
+    print ("Room changed to " + roomSelected.get())
+    
+    query = "SELECT * FROM roominformation i JOIN room_type t ON i.roomType = t.roomId WHERE i.roomNum = '" + roomSelected.get() + "'"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    if(len(result) > 0):
+        print(result)
+        room_selectInfo(result)
+    else:
+        print("No descriptions available")
+        messagebox.showwarning("Warning", "No descriptions available")
+
+########################### START OF THE PROGRAM ################################
 
 global main_screen
 main_screen = Tk()   # create a GUI window 
-main_screen.geometry("300x600") # set the configuration of GUI window 
+main_screen.geometry("500x300") # set the configuration of GUI window 
 main_screen.title("Room Information") # set the title of GUI window
 
 # create a Form label 
@@ -71,72 +124,34 @@ Label(text="").pack()
 
 # Create an Exit Button
 Button(text="Back", height="2", width="30", command=toLogin).pack()
-Label(text="").pack()
- 
-#SQL FOR ROOMS
-def removeData():
-    #Remove values from combobox
-    roomChoice.delete(0, END)
-    # roomChoice.insert(0, "")
-    # selectRoom.set('') # Clear the selection 
-    # # selectRoom.delete(0,'end') # clear the selection
-    # # l1.config(text=selectRoom.get()+':'+ str(cb1.current())) # value & index
-
-
-def on_field_change(index, value, op):
-    counter=0
-    print ("combobox changed to " + floorSelected.get())
-    removeData()
-    
-    query = "SELECT * FROM roominformation WHERE roomFloor = '" + floorSelected.get() + "'"
-    cursor.execute(query)
-    result = cursor.fetchall()
-    if(len(result) > 0):
-        for row in result:
-            print(row[0])
-            roomChoice.insert(counter, row[0])
-            counter+=1
-        # selectRoom.values = roomChoice
-            
-    else:
-        print("Login failed!")
-        messagebox.showwarning("Failed", "Login failed!")
-        login_screen.destroy()
-    
+Label(text="").pack()    
 
 
 floorSelected = StringVar()
-roomChoice = StringVar()
+roomSelected = StringVar()
 
-floorSelected.trace('w',on_field_change)
-# roomChoice.trace('w',on_field_change)
+floorSelected.trace('w',floorChanged)
+roomSelected.trace('w',roomChanged)
              
-floorChoice = ['1','2','3']
+floorChoice = []
+floorChoice = getFloorsAvailable()
+print(floorChoice)
 Label(text="Floor").pack() 
-selectFloor = ttk.Combobox(height="10", textvar=floorSelected, width="30", values = floorChoice).pack()
+selectFloor = ttk.Combobox(height="10", textvar=floorSelected, width="30")
+selectFloor.config(value = floorChoice)
+selectFloor.pack()
 Label(text="").pack() 
 
-roomChoice = [1,2,3,4,5,6,7,8,9,0]
+roomChoice = []
 Label(text="Room").pack() 
-selectRoom = ttk.Combobox(height="10", textvar=roomChoice, width="30", values = roomChoice).pack()
+selectRoom = ttk.Combobox(height="10", textvar=roomSelected, width="30")
+selectRoom.config(value = [])
+selectRoom.pack()
+# selectRoom.bind("<<ComboboxSelected>>", roomChanged)
+
 Label(text="").pack() 
-# Show Room Informations Button 
-# Label(text="1st Floor").pack() 
-
-# Button(text="Room 11", height="2", width="15", command=lambda:toRoomInfo(11)).pack() 
-# Button(text="Room 12", height="2", width="15", command=lambda:toRoomInfo(12)).pack()
-# Button(text="Room 13", height="2", width="15", command=lambda:toRoomInfo(13)).pack()
-# Button(text="Room 14", height="2", width="15", command=lambda:toRoomInfo(14)).pack()
-# Button(text="Room 15", height="2", width="15", command=lambda:toRoomInfo(15)).pack()
- 
 
 
-# Label(text="2nd Floor").pack() 
-# Button(text="Room 21", height="2", width="15", command=lambda:toRoomInfo(21)).pack() 
-# Button(text="Room 22", height="2", width="15", command=lambda:toRoomInfo(22)).pack()
-# Button(text="Room 23", height="2", width="15", command=lambda:toRoomInfo(23)).pack()
-# Button(text="Room 24", height="2", width="15", command=lambda:toRoomInfo(24)).pack()
-# Button(text="Room 25", height="2", width="15", command=lambda:toRoomInfo(25)).pack()
 
 
 
