580e8c0e64e017112a41a5ab697df7c42603af04
valjuniel123 <valjuniel123@gmail.com>
2022-05-02 21:19:23 +0800

    Added Checkout Prompt

--- checkout.py
+++ checkout.py
@@ -19,6 +19,22 @@ def toLogin():
     print("Logged out!")
     main_screen.destroy()
     
+def checkOutPrompt():
+    query = "SELECT * FROM roomreservation WHERE roomNum = '" + roomNum.get() + "' AND guestKey = '" + guestKey.get() + "' AND status = 1"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    if(len(result) > 0):
+        #Are you sure you want to check out?
+        if messagebox.askyesno("Check Out", "Are you sure you want to check out?"):
+            query2 = "UPDATE roomreservation SET status = 0 WHERE roomNum = '" + roomNum.get() + "' AND guestKey = '" + guestKey.get() + "' AND status = 1"
+            cursor.execute(query2)
+            con.commit()
+            messageBox.showinfo("Check Out", "Checked out! \nThank you for using Angeli's Hotel Reservation System!")
+    else:
+        messagebox.showwarning("Warning", "No reservation found!")
+    
+    
+    
 
 global main_screen
 main_screen = Tk()   # create a GUI window 
@@ -29,7 +45,23 @@ main_screen.title("Room Reservation") # set the title of GUI window
 Label(text="Angeli's Hotel Reservation System", bg="blue", width="300", height="1", font=("Calibri", 20)).pack() 
 Label(text="Checkout", bg="blue", width="300", height="1", font=("Calibri", 20)).pack() 
 Label(text="").pack() 
- 
+
+roomNum = StringVar()
+guestKey = StringVar()
+
+
+Label(text="Room Number * ").pack()
+roomNum_entry = Entry(width="30", textvariable=roomNum)
+roomNum_entry.pack()
+
+Label(text="").pack()
+Label(text="Guest Key * ").pack()
+guestKey_entry = Entry(width="30", textvariable=guestKey, show= '*')
+guestKey_entry.pack()
+
+Label(text="").pack()
+Button(text="Checkout", height="2", width="30", command=checkOutPrompt).pack()
+
 # Create an Exit Button
 Button(text="Back", height="2", width="30", command=toLogin).pack()
 Label(text="").pack() 
